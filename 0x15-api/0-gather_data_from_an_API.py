67d306772734e1a257197d2d8e6114e27944ac14
MakamuT <makamuthe2@gmail.com>
2024-06-03 17:54:50 +0000

    API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -10,15 +10,16 @@ REST_API = "https://jsonplaceholder.typicode.com"
 if __name__ == '__main__':
    if len(sys.argv) > 1:
       if re.fullmatch(r'\d+', sys.argv[1]):
-         id = int(sys.argv[1])
-         request = requests.get('{}/users/{}'.format(REST_API, id)).json()
+         user_id = int(sys.argv[1])
+         request = requests.get('{}/users/{}'.format(REST_API, user_id)).json()
          task_request = requests.get('{}/todos'.format(REST_API)).json()
          employee_name = request.get('name')
-         tasks = list(filter(lambda x: x.get('userID') == id, task_request))
+         tasks = list(filter(lambda x: x.get('userId') == user_id, task_request))
          completed_tasks = list(filter(lambda x: x.get('completed'), tasks))
+
          print(
               'Employee {} is done with taks({}/{}):'.format(
-		emp_name,
+		employee_name,
 		len(completed_tasks),
 		len(tasks)
 		)
