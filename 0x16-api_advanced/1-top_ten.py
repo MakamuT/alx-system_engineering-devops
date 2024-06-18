0c52beb9dd5fcb8d3d2d108ace9dbaee9d259b4e
MakamuT <makamuthe2@gmail.com>
2024-06-18 21:15:36 +0000

    advanced api

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,12 +9,12 @@ import requests
 
 
 def top_ten(subreddit):
-   """return top ten posts"""
-   user = {'User-Agent': 'Lizzie'}
-   url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'.
+    """return top ten posts"""
+    user = {'User-Agent': 'Lizzie'}
+    url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'.
                       format(subreddit), headers=user).json()
-   try:
+    try:
        for post in url.get('data').get('children'):
            print(post.get('data').get('title'))
-   except Exception:
+    except Exception:
       print(None)
