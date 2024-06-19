1bef278d96ae01bb254003f653b6e3db84b3be5e
MakamuT <makamuthe2@gmail.com>
2024-06-19 12:28:31 +0000

    advanced api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,16 +6,12 @@ import requests
 
 def number_of_subscribers(subreddit):
    """total subscribers"""
-   url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-   headers = {"User-Agent": "Mozilla/5.0"}
-   try:
-       response = requests.get(url, headers=headers, allow_redirects=False)
-       if response.status_code == 200:
-           data = response.json()
-           subscribers = data['data']['subscribers']
-           return subscribers
-       else:
-           return 0
-   except requests.RequestException as e:
-       print("An error occurred: {}".format(e))
-       return 0
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['dat']['subscibers']
+        return subscribers
+    else:
+        return 0
