03be394837b1f658993a21612553b6ace4dbffc7
MakamuT <makamuthe2@gmail.com>
2024-06-20 18:14:33 +0000

    advanced api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,15 +1,24 @@
 #!/usr/bin/python3
+"""
+queries the Reddit API and returns the number of subscribers
+(not active users, total subscribers)
+"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """total subscribers"""
+"""total number of subscribers"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
+    headers = {
+        "User-Agent": "0x16-api_advanced:project:v1.0.0 (by /u/firdaus_cartoon_jr)"
+    }
+    response = requests.get(url, headers=headers, allow_redirection=False)
+
+    if response.status_code != 200:
+        return 0
+
+    try:
+        data = response.json().get("data")
+        return data.get("subscribers", 0)
+    except (ValueError, AttributeError):
         return 0
