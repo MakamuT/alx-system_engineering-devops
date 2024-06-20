3c294e5b228260f83ec8a0502846b85df68fbb5b
MakamuT <makamuthe2@gmail.com>
2024-06-20 18:00:40 +0000

    advanced api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-"""queries the Reddit API and returns the number of subscribers"""
-
 import requests
 
 
 def number_of_subscribers(subreddit):
-   """total subscribers"""
+    """total subscribers"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
